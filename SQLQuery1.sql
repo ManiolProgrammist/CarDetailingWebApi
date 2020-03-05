1aae7ad18a03fc31bd3ffd23a197f92aa1dd2a5c
p.trzeciak666@gmail.com <Rowerto110->
2020-03-05 14:26:04 +0100

    sql query

---
+++ SQLQuery1.sql
@@ -0,0 +1,176 @@
+use CarCosmeticSalon;
+drop table Users;
+go
+drop table Images;
+go
+drop table OrdersTemplateImages;
+go
+drop table Orders;
+go
+
+drop table OrdersTemplate;
+go
+drop table OrdersInformation;
+go
+drop table OrdersTemplateInformation;
+go
+drop table UserInformations;
+go
+drop table SmallImages;
+go
+drop table UserImages;
+go
+drop table UserType;
+go
+
+Create Table UserType(
+UserTypeId int not null IDENTITY    PRIMARY KEY,
+AccessRights int not null, -- 1 temporary user,2 user,3 employee,4 admin
+Name nvarchar(255) not null
+);
+
+Create Table Users (
+UserId int NOT NULL IDENTITY    PRIMARY KEY,
+Login nvarchar(254) NOT NULL,
+Password nvarchar(254) NOT NULL,
+Email nvarchar(254) NOT NULL,
+FirstName nvarchar(254) NOT NULL,
+Surname nvarchar(254) NOT NULL,
+PhoneNumber varchar(15) NOT NULL,
+AccoutCreateDate DATETIME NOT NULL,
+UserTypeId int foreign key references UserType(UserTypeID));
+
+
+--Create Table Users (
+--UserId int NOT NULL IDENTITY    PRIMARY KEY,
+--Login nvarchar(254) NOT NULL,
+--Password nvarchar(254) NOT NULL,
+--Email nvarchar(254) NOT NULL,
+--FirstName nvarchar(254) NOT NULL,
+--Surname nvarchar(254) NOT NULL,
+--PhoneNumber varchar(15) NOT NULL,
+--AccoutCreateDate DATETIME NOT NULL,
+--IsAdmin bit NOT NULL,
+--IsEmployee bit NOT NULL,
+--IsTemporary bit NOT NULL);
+
+Create Table OrdersTemplate(
+OrderTemplateId int not null identity primary key,
+MaxCost money not null,
+MinCost money not null,
+Name nvarchar(500) not null,
+AdditionalInformation nvarchar(3000),
+ExpectedTime time);
+
+
+
+
+
+
+
+Create Table Orders(
+OrderId int NOT NULL IDENTITY    PRIMARY KEY,
+UserId  int NOT NULL foreign key references Users(UserId),
+CreateOrderUserId  int NOT NULL foreign key references Users(UserId),
+OrderTemplateId  int NOT NULL foreign key references OrdersTemplate(OrderTemplateId),
+OrderDate DATETIME NOT NULL,
+ExpectedStartOfOrder DATE NULL,
+CompletedOrderDate DATETIME NULL,
+Cost money null,
+IsOrderCompleted bit not null,
+IsOrderStarted bit not null);
+
+
+
+
+Create Table UserInformations(
+InformationId int not null identity primary key,
+UserId  int foreign key references Users(UserId),
+TypeOfInformation nvarchar(254) not null, 
+Information nvarchar(max));
+
+
+Create Table OrdersInformation(
+InformationId int not null identity primary key,
+OrderId int not null foreign key references Orders(OrderId),
+TypeOfInformation nvarchar(254) not null, 
+Information nvarchar(max));
+
+
+Create Table OrdersTemplateInformation(
+InformationId int not null identity primary key,
+OrderTemplateId int not null foreign key references OrdersTemplate(OrderTemplateId),
+TypeOfInformation nvarchar(254) not null, 
+Information nvarchar(max));
+
+
+create table UserImages(
+ImageId int not null identity primary key,
+UserId int not null foreign key references Users(UserId),
+AdditionalInformation nvarchar(254),
+Image image)
+
+--bo mo¿e byæ parê image id do templatea
+
+create table OrdersTemplateImages(
+ImageId int not null identity primary key,
+OrderTemplateId int not null foreign key references OrdersTemplate(OrderTemplateId),
+AdditionalInformation nvarchar(254),
+Image image)
+
+Create Table Blog(
+BlogId int not null identity primary key,
+content nvarchar(MAX));
+
+Create Table BlogImages(
+ImageId int not null identity primary key,
+BlogId int not null foreign key references Blog(BlogId),
+Image image);
+
+Create Table SocialMedia(
+SocialMediaId int not null identity primary key,
+ShortDescripption nvarchar(254),
+TypeOfSocialmedia nvarchar(100),
+link nvarchar(MAX) not null);
+
+
+
+Insert into OrdersTemplate(MaxCost,MinCost,Name,AdditionalInformation,ExpectedTime) values (500,300,'test 1', 'information for test1','20:30:00');
+Insert into OrdersTemplate(MaxCost,MinCost,Name,AdditionalInformation,ExpectedTime) values (500,300,'test 2', 'information for test2','00:30:00');
+Insert into OrdersTemplate(MaxCost,MinCost,Name,AdditionalInformation,ExpectedTime) values (100,50,'test 3', 'information for test3','00:01:00');
+
+Insert Into UserType(AccessRights,Name) values (1,'Temporary User');
+Insert Into UserType(AccessRights,Name) values (2,'Normal User');
+Insert Into UserType(AccessRights,Name) values (3,'Employee');
+Insert Into UserType(AccessRights,Name) values (4,'Admin');
+
+select * from UserType;
+
+Insert Into Users(
+Login,Password,Email,FirstName,Surname,PhoneNumber,AccoutCreateDate,UserTypeId) 
+values ('Admin','Admin','p.trzeciak666@gmail.com','Patryk','Trzeciak',
+'790821390',GETDATE(),4);
+Insert Into Users(
+Login,Password,Email,FirstName,Surname,PhoneNumber,AccoutCreateDate,UserTypeId ) 
+values ('User1','User1','p.trzeciak666@gmail.com','Patryk1','Trzeciak1',
+'790821391',GETDATE(),2);
+Insert Into Users(
+Login,Password,Email,FirstName,Surname,PhoneNumber,AccoutCreateDate,UserTypeId ) 
+values ('Employee1','Employee1','p.trzeciak666@gmail.com','Patryk1','Trzeciak1',
+'790821391',GETDATE(),3);
+select * from users;
+
+
+Insert Into Orders
+(UserId,CreateOrderUserId,OrderTemplateId,OrderDate,Cost,IsOrderCompleted,IsOrderStarted)
+Values(2,2,3,GETDATE(),100,0,0);
+Insert Into Orders
+(UserId,CreateOrderUserId,OrderTemplateId,OrderDate,Cost,IsOrderCompleted,IsOrderStarted)
+Values(3,1,2,GETDATE(),100,0,0);
+Insert Into Orders
+(UserId,CreateOrderUserId,OrderTemplateId,OrderDate,Cost,IsOrderCompleted,IsOrderStarted)
+Values(2,1,1,GETDATE(),100,0,0);
+
+
+select * from Orders;
+
